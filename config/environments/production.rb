258e0fe8f986a10b227840fe617eef8fca567129
lauradavila-ctl <laura.davila@austin.utexas.edu>
2012-02-10 16:54:54 -0600

    Precompile Blueprint CSS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ SampleApp::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Precompile Blueprint CSS
+  config.assets.precompile += [ "blueprint/*.css" ]
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
